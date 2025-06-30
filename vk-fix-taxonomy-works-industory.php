c8063cdd3ca278cbdf1797886ff9e10f48a37a3e
kurudrive <kurudrive@gmail.com>
2025-07-01 01:27:15 +0900

    update discription

--- vk-fix-taxonomy-works-industory.php
+++ vk-fix-taxonomy-works-industory.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: VK Fix Taxonomy Works Industory
-Description: 2025年6月30日以前に VK FullSite Installer から X-T9 Pro ビジネスのサイトをインポートしたサイトで、カスタム投稿タイプ制作実績（works）のカスタムタクソノミー works-Industry を使用している場合に謝動作を引き起こすため、works-Industry をworks-industry に書き換えるためのプラグインです。一度有効化してダッシュボードを開いたら処理が完了するので停止・削除してください。
+Description: 2025年6月30日以前に VK FullSite Installer から「X-T9 Pro 版ビジネス」のサイトをインポートしたサイトで、カスタム投稿タイプ制作実績（works）のカスタムタクソノミー works-Industry をそのまま使用している場合に誤動作を引き起こすため、works-Industry をworks-industry に書き換えるためのプラグインです。一度有効化してダッシュボードを開いたら処理が完了するので停止・削除してください。
 Author: Vektor,Inc.
 Version: 1.0.0
 License: GPLv2
